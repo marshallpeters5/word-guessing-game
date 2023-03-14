a38fbcda09ca8f5f003f26a40e6e3dbaa8e74a59
Marshall <marshall.peters01@gmail.com>
2023-03-13 21:07:02 -0400

    added initial functions and arrays

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,57 @@
+var wordBlank = [];
+var possibleWords = ["Function", "JavaScript", "Document", "Index", "Variable", "Stylesheet"];
+var chosenWord = [];
+var chosenLetters = [];
+document.querySelector(".word-blanks");
 
+function init() {
 
-function startGame (){
+}
+
+function startGame() {
+
+}
+
+function winGame() {
+
+}
+
+function loseGame() {
+
+}
+
+function setTime() {
+
+}
+
+function renderBlanks() {
+
+}
+
+function setWins() {
+
+}
+
+function setLosses() {
+
+}
+
+function getWins() {
+
+}
+
+function getLosses() {
+
+}
+
+function setWins() {
+
+}
+
+function setLosses() {
     
+}
+
+function resetGame() {
+
 }
