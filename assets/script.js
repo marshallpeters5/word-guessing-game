2c64be9a4e0d53621a53bc217d72bf06bf534faa
Marshall <marshall.peters01@gmail.com>
2023-03-13 21:31:48 -0400

    set time and other functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
-var wordBlank = [];
-var possibleWords = ["Function", "JavaScript", "Document", "Index", "Variable", "Stylesheet"];
+var timerEl = document.getElementById('countdown');
+var startButton = document.querySelector(".start-button");
+var wordBlank = document.querySelector(".word-blanks");
+var possibleWords = ["Modulus", "Function", "Array", "Boolean", "String", "JavaScript", "Document", "Index", "Variable", "Stylesheet", "Object"];
 var chosenWord = [];
 var chosenLetters = [];
 document.querySelector(".word-blanks");
 
 function init() {
-
+    getLosses();
+    getWins();
 }
 
 function startGame() {
-
+ isWin = false;
 }
 
 function winGame() {
-
+    wordBlank.textContent = "You Win!";
+    winCounter++
+    startButton.disabled = false;
 }
 
 function loseGame() {
@@ -21,11 +26,30 @@ function loseGame() {
 }
 
 function setTime() {
-
+    function countdown() {
+        var timeLeft = 10;
+      
+        var timeInterval = setInterval(function () {
+          if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+          } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+          } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            winGame();
+          }
+        }, 1000);
+      }
 }
 
 function renderBlanks() {
-
+    chosenWord = words[Math.floor(Math.random() * words.length)];
+    chosenLetters = chosenWord.length;
+    wordBlank = chosenLetters.length;
+    blankLetters = []
 }
 
 function setWins() {
@@ -42,14 +66,6 @@ function getWins() {
 
 function getLosses() {
 
-}
-
-function setWins() {
-
-}
-
-function setLosses() {
-    
 }
 
 function resetGame() {
